7a3a151a2ef5e343a5a614e4d9a1de01fbbad98a
Rusaln-IT <ruslan.r.f@yandex.ru>
2025-12-16 19:37:35 +0500

    commit admin panel category

--- run_parsers.sh
+++ run_parsers.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-# Скрипт для запуска Laravel queue на Beget
-
-# Переходим в директорию проекта
-cd /home/info90zj/info90zj.beget.tech || exit
-
-# Запускаем очередь на 1 раз, только для нужной очереди
+# Путь из команды pwd на сервере
+cd /home/i/info90zj/info90zj.beget.tech || exit
 /usr/local/bin/php8.3 artisan queue:work --queue=parsers --once
+
