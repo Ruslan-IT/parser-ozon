4fd5a6771ef7207edcefe6a6b774c206f6272828
Rusaln-IT <ruslan.r.f@yandex.ru>
2025-12-16 22:58:04 +0500

    commit admin panel category

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'livewire/*',
         ]);
+
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
