44be1c191e697ddd1eba8f3317c8ad55f24653cd
Rusaln-IT <ruslan.r.f@yandex.ru>
2025-12-17 02:36:49 +0500

    commit admin panel category

--- run_parsers.sh
+++ run_parsers.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-# Путь из команды pwd на сервере
-cd /home/i/info90zj/info90zj.beget.tech || exit
-/usr/local/bin/php8.3 artisan queue:work --queue=parsers --once
+cd ~/info90zj.beget.tech
 
+# Логируем начало работы
+echo "=== Starting parser worker at $(date) ===" >> ~/parser-worker.log
+
+# Запускаем воркер
+/usr/local/bin/php8.3 artisan queue:work \
+  --queue=parsers \
+  --sleep=3 \
+  --tries=3 \
+  --timeout=180 \
+  --memory=128 \
+  --stop-when-empty
+
+# Логируем завершение
+echo "=== Parser worker finished at $(date) ===" >> ~/parser-worker.log
