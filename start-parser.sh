07438eb38b4cd527cbcd10904d4b284b041e915e
Rusaln-IT <ruslan.r.f@yandex.ru>
2025-12-17 02:39:41 +0500

    commit admin panel category

---
+++ start-parser.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd ~/info90zj.beget.tech
+
+# Логируем начало работы
+echo "=== Starting parser worker at $(date) ===" >> ~/parser-worker.log
+
+# Запускаем воркер
+/usr/local/bin/php8.3 artisan queue:work \
+  --queue=parsers \
+  --sleep=3 \
+  --tries=3 \
+  --timeout=180 \
+  --memory=128 \
+  --stop-when-empty
+
+# Логируем завершение
+echo "=== Parser worker finished at $(date) ===" >> ~/parser-worker.log
