ce23acb579bac52b9aa2c17bcd102ba195e9d4c6
Rusaln-IT <ruslan.r.f@yandex.ru>
2025-12-17 23:57:02 +0500

    commit admin panel category

---
+++ run_parsers_cron.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /home/info90zj/info90zj.beget.tech || exit 1
+
+/usr/local/bin/php8.3 artisan parsers:run-all
+/usr/local/bin/php8.3 artisan queue:work --queue=parsers --sleep=3 --stop-when-empty
